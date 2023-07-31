7b861402f655a962002e762647662f5736381c3b
우철 <2019041421@hanyang.ac.kr>
2023-07-31 17:59:39 +0900

    code modify

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import DestinationPage from './router/pages/Destination/';
-import CrewPage from './router/pages/crew/';
-import TechnologyPage from './router/pages/technology/';
-import HomePage from './router/pages/Home/';
-import MapPage from './router/pages/map/';
+import DestinationPage from './router/pages/Destination';
+import CrewPage from './router/pages/crew';
+import TechnologyPage from './router/pages/technology';
+import HomePage from './router/pages/Home';
+import MapPage from './router/pages/map';
 
 const router = createBrowserRouter([
   {
