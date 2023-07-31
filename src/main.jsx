30b441710996c9c062a8b5e51d426aca2e505e5e
우철 <2019041421@hanyang.ac.kr>
2023-07-31 18:12:02 +0900

    destination file modify

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import DestinationPage from './router/pages/Destination/index.jsx';
+import DestinationPage from './router/pages/Destination/';
 import CrewPage from './router/pages/crew';
 import TechnologyPage from './router/pages/technology';
 import HomePage from './router/pages/Home';
@@ -15,30 +15,30 @@ const router = createBrowserRouter([
     path: '/',
     element: <App />,
     children: [
-      {
-        index: true,
-        elemet: <HomePage />,
-      },
-      {
-        path: 'home',
-        element: <HomePage />,
-      },
       // {
-      //   path: 'destination',
-      //   element: <DestinationPage />,
+      //   index: true,
+      //   elemet: <HomePage />,
+      // },
+      // {
+      //   path: 'home',
+      //   element: <HomePage />,
       // },
       {
-        path: 'crew',
-        element: <CrewPage />,
+        path: 'destination',
+        element: <DestinationPage />,
       },
-      {
-        path: 'technology',
-        element: <TechnologyPage />,
-      },
-      {
-        path: 'map',
-        element: <MapPage />,
-      },
+      // {
+      //   path: 'crew',
+      //   element: <CrewPage />,
+      // },
+      // {
+      //   path: 'technology',
+      //   element: <TechnologyPage />,
+      // },
+      // {
+      //   path: 'map',
+      //   element: <MapPage />,
+      // },
     ],
   },
 ]);
