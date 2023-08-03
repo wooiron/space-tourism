ed9e276cfa55bca5299ff5618ac28ff46cb96609
우철 <2019041421@hanyang.ac.kr>
2023-08-03 11:21:58 +0900

    map 추가

--- src/router/pages/map/index.jsx
+++ src/router/pages/map/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 // for kakao map api
 const MapPage = () => {
-  return <div>MapPage</div>;
+  return (
+    <Map
+      center={{ lat: 33.5563, lng: 126.79581 }}
+      style={{ width: '100%', height: '360px' }}
+    >
+      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+        <div style={{ color: '#000' }}>Hello World!</div>
+      </MapMarker>
+    </Map>
+  );
 };
 
 export default MapPage;
